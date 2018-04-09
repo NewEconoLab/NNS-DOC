f7b5ce35dbe2bfe0e33b0f6e7be70acf23f07233
nuanbing <jinghui@wayne.edu>
2018-04-09 20:37:40 +0800

    disable latex

--- conf.py
+++ conf.py
@@ -136,10 +136,10 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'NNS.tex', 'NNS Documentation',
-     'Jinghui Liao', 'manual'),
-]
+# latex_documents = [
+#     (master_doc, 'NNS.tex', 'NNS Documentation',
+#      'Jinghui Liao', 'manual'),
+# ]
 
 
 # -- Options for manual page output ------------------------------------------
